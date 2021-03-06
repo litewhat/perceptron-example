4057f2036e066ebca7561bba5242b045eb2ad475
Paweł Zielonka <lite.what@gmail.com>
2021-03-06 11:06:04 +0100

    Created project from template

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to perceptron-example
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/litewhat/perceptron_example_test.clj
@@ -0,0 +1,7 @@
+(ns litewhat.perceptron-example-test
+  (:require [clojure.test :refer :all]
+            [litewhat.perceptron-example :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
